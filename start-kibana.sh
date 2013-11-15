1eac63cd987b26d3b795aa99fc77002d0ff7cbac
Andrew Hodgson <ah9@me.com>
2013-11-15 17:10:12 +0000

    Update config.js to latest, and provide override Docker volume

--- start-kibana.sh
+++ start-kibana.sh
@@ -10,17 +10,49 @@ test -n "${ES_PROTO}" || exit 1
 test -n "${ES_HOST}"  || exit 1
 test -n "${ES_PORT}"  || exit 1
 
-cat > /src/kibana/config.js <<EOS
-var config = new Settings(
-{
-  elasticsearch: "${ES_PROTO}://${ES_HOST}:${ES_PORT}",
-  kibana_index:  "kibana-int", 
-  modules:       ['histogram','map','pie','table','filtering',
-                 'timepicker','text','fields','hits','dashcontrol',
-                 'column','derivequeries','trends','bettermap','query'],
-  }
-);
+CONTAINER_KIBANA_ROOT=/src/kibana
+LOCAL_KIBANA_VOLUME=/var/lib/kibana
+
+cat > ${CONTAINER_KIBANA_ROOT}/src/config.js <<EOS
+define(['settings'],
+function (Settings) {
+  "use strict";
+
+  return new Settings({
+    elasticsearch: "${ES_PROTO}://${ES_HOST}:${ES_PORT}",
+    kibana_index: "kibana-int",
+    panel_names: [
+      'histogram',
+      'map',
+      'pie',
+      'table',
+      'filtering',
+      'timepicker',
+      'text',
+      'fields',
+      'hits',
+      'dashcontrol',
+      'column',
+      'derivequeries',
+      'trends',
+      'bettermap',
+      'query',
+      'terms',
+      'sparklines'
+    ]
+  });
+});
 EOS
+
+# This gives an installer the ability to overwrite the container's
+# Kibana code with a local copy, mounted as a Docker volume to
+# $LOCAL_KIBANA_VOLUME.
+#
+# For example, set the default dashboard by creating a file called
+# /var/lib/kibana/dashboards/default.json
+if [ -d ${LOCAL_KIBANA_VOLUME} ]; then
+  rsync -av ${LOCAL_KIBANA_VOLUME}/* ${CONTAINER_KIBANA_ROOT}/src/app/
+fi
 
 exec /usr/sbin/nginx -c /etc/nginx/nginx.conf
 
